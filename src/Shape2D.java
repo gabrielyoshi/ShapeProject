a4626aa35fb4acdb8b514c6f7e23b266848fc559
claudiagabe <claudiagabe@localhost>
2025-09-30 18:31:22 -0700

    Refactored Shape hierarchy into Shape, Shape2D, Shape3D

--- src/Shape2D.java
+++ src/Shape2D.java
@@ -1,4 +1,24 @@
 
-public class Shape2D {
+public abstract class Shape2D extends Shape {
+	
+	private static final int DEFAULT_HEIGHT = 0; // z-axis set to 0 for 2D shapes
+	
+	// Overloaded Constructor for 2D shapes
+	
+	public Shape2D(int length, int width) {
+		super(length, width, DEFAULT_HEIGHT);
+	}
+	
+	// Calculates area of 2D shapes
+	
+	public int calculateArea() {
+		return getLength() * getWidth();
+	}
+	
+	// Calculates perimeter of 2D shapes
+	
+	public int calculatePerimeter() {
+		return 2*(getLength() + getWidth());
+	}
 
 }
--- src/Shape3D.java
+++ src/Shape3D.java
@@ -1,4 +1,15 @@
 
-public class Shape3D {
+public abstract class Shape3D extends Shape {
 
+	// Constructor for 3D shapes inherits from Shape constructor
+	
+	public Shape3D(int length, int width, int height) {
+		super(length, width, height);
+	}
+	
+	// Calculates volume of all shapes (note: only 3D shapes return value
+	
+	public int calculateVolume() {
+		return getLength() * getWidth() * getHeight();
+	}
 }
