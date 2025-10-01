841fd490113b5648d62ec603317edd6f05d890cc
claudiagabe <claudiagabe@localhost>
2025-09-30 18:28:37 -0700

    Refactored Shape hierarchy into Shape, Shape2D, Shape3D

--- src/Shape.java
+++ src/Shape.java
@@ -1,33 +1,27 @@
 
-public class Shape {
+public abstract class Shape {
 	
 	// Dimensions that describe size
 	private int length; // x-axis
 	private int width; // y-axis
 	private int height; // z-axis
 	
-	private static final int DEFAULT_HEIGHT = 0; // z-axis set to 0 for 2D shapes
 	private static int numShapes = 0; // counts number of shape objects
 	
+	// Returns a text description that lists properties of shape
 	protected void printProperties() {
 		System.out.println("Length (x-axis) = " + length);
 		System.out.println("Width (y-axis) = " + width);
 		System.out.println("Height (z-axis) = " + height);
 	}
 	
-	// Constructor for 3D shapes
+	// Constructor for any shape
 	
 	public Shape(int length, int width, int height) {
 		this.length = length;
 		this.width = width;
 		this.height = height;
 		numShapes++;
-	}
-	
-	// Overloaded Constructor for 2D shapes
-	
-	public Shape(int length, int width) {
-		this(length, width, DEFAULT_HEIGHT);
 	}
 	
 	// Accessors for dimensions
@@ -54,33 +48,6 @@ public class Shape {
 			throw new IllegalArgumentException("Height must be positive.");
 		}
 		this.height = height;}
-	
-	// Calculates area of 2D shapes
-	
-	public int calculateArea() {
-		if (height != 0) {
-			throw new UnsupportedOperationException("3D shapes do not have area.");
-		}
-		return length * width;
-	}
-	
-	// Calculates perimeter of 2D shapes
-	
-	public int calculatePerimeter() {
-		if (height != 0) {
-			throw new UnsupportedOperationException("3D shape perimeter not supported.");
-		}
-		return 2*(length + width);
-	}
-	
-	// Calculates volume of all shapes (note: only 3D shapes return value
-	
-	public int calculateVolume() {
-		if (height == 0) {
-			throw new UnsupportedOperationException("2D shapes do not have volume.");
-		}
-		return length * width * height;
-	}
 	
 	// method that returns a text description of shape object
 	@Override
